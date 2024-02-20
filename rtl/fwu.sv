f11c4aa6d674c75464c5f813c85486ee527a3297
Riccardo Fusari <ric.fusari@gmail.com>
2024-02-20 15:21:56 +0100

    Automated sync:  M rtl/fwu.sv

--- rtl/fwu.sv
+++ rtl/fwu.sv
@@ -9,16 +9,16 @@ module fwu (
 
 always_comb begin
     if (EX_MEM_dest == src1)
-        ForwardA <= 10; //The first ALU op is forwarded from the prior ALU result
+        ForwardA = 10; //The first ALU op is forwarded from the prior ALU result
     else if (EX_MEM_dest == src2)
-        ForwardB <= 10;
+        ForwardB = 10;
     else if (MEM_WB_dest == src1)
-        ForwardA <= 01; //The first ALU op is forwarded drom data memory or an earlier ALU result
+        ForwardA = 01; //The first ALU op is forwarded drom data memory or an earlier ALU result
     else if (MEM_WB_dest == src2)
-        ForwardB <= 01;
+        ForwardB = 01;
     else
-        ForwardA <= 00;
-        ForwardB <= 00;
+        ForwardA = 00;
+        ForwardB = 00;
 end
 
 endmodule
